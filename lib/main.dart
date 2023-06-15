ab83765ebf78453d4eee2a02cefb10ed6e0f6479
Wathiq <wathiq@atu.edu.iq>
2023-06-15 17:59:10 +0300

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Wathiq Flutter Project'),
     );
   }
 }
